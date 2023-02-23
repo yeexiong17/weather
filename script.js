3fbd055fb4a6497080b3fbcc5a7b1a75a8db1739
yeexiong17 <wyeexiong17@gmail.com>
2023-02-24 00:43:16 +0800

    changed the ui

--- script.js
+++ script.js
@@ -214,3 +214,14 @@ input.addEventListener('keypress', function myFunc() {
         input.value = "";
     }
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".nav-bar").style.top = "0";
+    } else {
+        document.querySelector(".nav-bar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
